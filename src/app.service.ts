43403acdc9bddd5f1e81effc481065ff0259e522
jatin10269 <81247242+jatin10269@users.noreply.github.com>
2023-01-11 13:55:11 -0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World v9!!';
+    return 'Hello World v10!!';
   }
 }
